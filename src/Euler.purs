fa64dde43dbe063f825494cd8036a95c7a72aad9
Feihong Hsu <feihong.hsu@gmail.com>
2017-07-20 19:20:03 -0500

    Add solution to Project Euler #1

---
+++ src/Euler.purs
@@ -0,0 +1,9 @@
+module Euler where
+
+import Prelude
+import Data.List (range, filter)
+import Data.Foldable (sum)
+
+euler1 :: Int
+euler1 =
+  sum $ filter (\n -> mod n 3 == 0 || mod n 5 == 0) $ range 0 999
--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,9 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Euler (euler1)
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
+  log $ "Answer is " <> show euler1 
