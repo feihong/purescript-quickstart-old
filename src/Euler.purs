3d99c153ba542c9ba48a82b5427a264562206f66
Feihong Hsu <feihong.hsu@gmail.com>
2018-07-18 11:10:05 -0500

    Add mod operator test

--- src/Euler.purs
+++ src/Euler.purs
@@ -6,4 +6,6 @@ import Data.Foldable (sum)
 
 euler1 :: Int
 euler1 =
-  sum $ filter (\n -> mod n 3 == 0 || mod n 5 == 0) $ range 0 999
+  sum 
+  $ filter (\n -> mod n 3 == 0 || mod n 5 == 0) 
+  $ range 0 999
--- test/Main.purs
+++ test/Main.purs
@@ -1,19 +1,21 @@
 module Test.Main where
 
 import Prelude
+
 import Effect (Effect)
+import Euler (euler1)
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
 
-import Euler (euler1)
-
 main :: Effect Unit
 main = run [consoleReporter] do
-  describe "Hello Test" do
+  describe "Arithmetic" do
     it "addition" do
       (1 + 2) `shouldEqual` 3
+    it "mod" do
+      (77 `mod` 10) `shouldEqual` 7
 
   describe "Project Euler" do
     it "#1" do
