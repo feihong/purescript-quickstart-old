a86e314cdd7f603920163e0194695ab02cfbbfe0
Feihong Hsu <feihong.hsu@gmail.com>
2018-07-19 17:22:12 -0500

    Clean up chapter 3 tests

--- test/Chapter3Tests.purs
+++ test/Chapter3Tests.purs
@@ -1,22 +1,27 @@
 module Chapter3Tests where
 
-import Chapter3
-import Prelude
+import Prelude (Unit, discard)
 
-import Chapter3 (nameAppears, removeDuplicates)
+import Chapter3 
 import Data.Array (toUnfoldable)
 import Data.List.Types ((:))
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
+addr1 :: Address
 addr1 = createAddress "123 Main St" "Chicago" "IL"
+addr2 :: Address
 addr2 = createAddress "456 Xintiao Lu" "Shenyang" "LN"
 
+entry1 :: Entry
 entry1 = createEntry "Bob" "Robot" addr1
+entry2 :: Entry
 entry2 = createEntry "Tom" "Voltron" addr2
 
+book1 :: AddressBook
 book1 = toUnfoldable [entry1, entry2]
 
+book2 :: AddressBook
 book2 = entry1 : book1
 
 spec :: Spec Unit
