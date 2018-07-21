fe1b1beaec504c400e0a360e3f24a09ee24c431d
Feihong Hsu <feihong.hsu@gmail.com>
2018-07-20 21:24:59 -0500

    Add more chapter 5 exercises

--- src/Chapter5.purs
+++ src/Chapter5.purs
@@ -1,6 +1,8 @@
 module Chapter5 where
 
 import Prelude
+import Data.Picture
+import Data.Maybe (Maybe(..))
 
 factorial :: Int -> Int
 factorial 0 = 1
@@ -29,3 +31,18 @@ sameCity {address: {city: c1}} {address: {city: c2}} = c1 == c2
 fromSingleton :: forall a. a -> Array a -> a
 fromSingleton _ [x] = x
 fromSingleton d _ = d
+
+origin :: Point
+origin = Point {x: 0.0, y: 0.0}
+
+circle :: Shape
+circle = Circle origin 10.0
+
+expandAndCenter :: Shape -> Shape
+expandAndCenter (Circle _ r) = Circle origin (r * 2.0)
+expandAndCenter (Rectangle _ w h) = Rectangle origin (w * 2.0) (h * 2.0)
+expandAndCenter x = x
+
+extractText :: Shape -> Maybe String
+extractText (Text _ text) = Just text
+extractText _ = Nothing
