db119cfabc990a6e693135a0ff849dc98909ffba
Feihong Hsu <feihong.hsu@gmail.com>
2018-07-23 09:16:51 -0500

    Move result definition into the do block where it's used
    Note the indentation

--- test/OperatorTests.purs
+++ test/OperatorTests.purs
@@ -8,17 +8,18 @@ import Data.String (trim)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
 
-result :: Int
-result = 
-  "   6     "
-  # trim  
-  # fromString
-  # fromMaybe 0
-  # (*) 2
-  # (_ + 1)     -- operator section
 
 spec :: Spec Unit
 spec = 
   describe "Operator tests" do
     it "applyFlipped operator #" do
+      let 
+        result = 
+          "   6     "
+          # trim  
+          # fromString
+          # fromMaybe 0
+          # (*) 2
+          # (_ + 1)     -- operator section
+               
       result `shouldEqual` 13
