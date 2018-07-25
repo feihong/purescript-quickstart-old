b26fd945db58ce29370613346c19c3d7114fe043
Feihong Hsu <feihong.hsu@gmail.com>
2018-07-25 09:56:59 -0500

    Add sequencing operator test

--- test/OperatorTests.purs
+++ test/OperatorTests.purs
@@ -3,10 +3,11 @@ module OperatorTests where
 import Prelude
 
 import Data.Int (fromString)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String (trim)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
+import Control.Apply ((*>), applySecond)
 
 
 spec :: Spec Unit
@@ -29,3 +30,11 @@ spec =
 
       map _.a arr `shouldEqual` [1, 2, 3]
       map _.b arr `shouldEqual` ["A", "B", "C"]
+    it "Sequencing operator *>" do
+      -- *> is actually shorthand for the applySecond function:
+      -- Combine two effectful actions, keeping only the result of the second
+      -- applySecond :: forall a b f. Apply f => f a -> f b -> f b
+      applySecond (Just 'a') (Just 2) `shouldEqual` Just 2
+      applySecond (Just 'a') (Nothing :: Maybe Int) `shouldEqual` Nothing
+      (Just 400 *> Just "foo") `shouldEqual` Just "foo"
+      (Just 401 *> (Nothing :: Maybe String)) `shouldEqual` Nothing
