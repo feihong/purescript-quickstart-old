3961ab3f2ae8239c695b46e52bb34ff9473eef03
Feihong Hsu <feihong.hsu@gmail.com>
2018-07-25 11:24:11 -0500

    Make sure state is two alphabetic characters

--- src/Chapter7.purs
+++ src/Chapter7.purs
@@ -9,7 +9,6 @@ import Data.String.Regex (Regex, test, regex)
 import Data.String.Regex.Flags (noFlags)
 import Data.Validation.Semigroup (V, invalid)
 import Partial.Unsafe (unsafePartial)
-import Data.String as S
 
 combineMaybe :: forall a f. Applicative f => Maybe (f a) -> f (Maybe a)
 combineMaybe Nothing = pure Nothing
@@ -21,21 +20,25 @@ nonEmpty :: String -> String -> V Errors Unit
 nonEmpty field "" = invalid ["Field '" <> field <> "' cannot be empty"]
 nonEmpty _     _  = pure unit
 
-lengthIs :: String -> Int -> String -> V Errors Unit
-lengthIs field len value | S.length value /= len =
-  invalid ["Field '" <> field <> "' must have length " <> show len]
-lengthIs _     _   _     =
-  pure unit
+stateRegex :: Regex
+stateRegex =
+  unsafePartial
+    case regex "^[a-zA-Z]{2}$" noFlags of
+      Right r -> r
+
+validateState :: String -> V Errors Unit
+validateState text =
+  case test stateRegex text of
+    true -> pure unit
+    false -> invalid ["Field state must be two alphabetic characters"]
 
 validateAddress :: Address -> V Errors Address
 validateAddress (Address o) =
   address <$> (nonEmpty "Street" o.street *> pure o.street)
           <*> (nonEmpty "City"   o.city   *> pure o.city)
-          <*> (lengthIs "State" 2 o.state *> pure o.state)
+          <*> (validateState o.state *> pure o.state)
 
-stateRegex :: Regex
-stateRegex =
-  unsafePartial
-    case regex "^[A-Z]{2}$" noFlags of
-      Right r -> r
-
+matches :: String -> Regex -> String -> V Errors Unit
+matches _ regex value | test regex value = pure unit
+matches field _ _ = invalid ["Field '" <> field <> 
+                    "' did not match the required format"]
--- test/Chapter7Tests.purs
+++ test/Chapter7Tests.purs
@@ -1,12 +1,20 @@
 module Chapter7Tests where
 
-import Chapter6
+import Chapter7
 import Prelude
 
 import Control.Apply (lift2)
+import Data.AddressBook (address)
 import Data.Maybe (Maybe(..))
+import Data.Validation.Semigroup (V, toEither)
 import Test.Spec (Spec, describe, it)
+import Data.Either (Either(..))
 import Test.Spec.Assertions (shouldEqual)
+
+getErrors :: forall a. V (Array String) a -> Array String
+getErrors v = case toEither(v) of 
+  Left errors -> errors
+  Right _ -> []
 
 spec :: Spec Unit
 spec = describe "Chapter 7" do
@@ -15,3 +23,10 @@ spec = describe "Chapter 7" do
     lift2 (*) (Just 4) (Just 2) `shouldEqual` Just 8
     lift2 (-) Nothing (Just 5) `shouldEqual` Nothing
     lift2 mod (Just 33) (Just 10) `shouldEqual` Just 3
+  it "Validate state is 2 alphabetic characters" do
+    (address "123 Main St" "Chicago" "Illinois" # validateAddress # getErrors)
+      `shouldEqual` 
+      ["Field state must be two alphabetic characters"]
+    (address "123 Main St" "Chicago" "IL" # validateAddress # getErrors)
+      `shouldEqual` 
+      []
